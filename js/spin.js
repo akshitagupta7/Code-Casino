e6ba4228b47185f24d47ecb3aa3ad84b004f7039
Akshita Gupta <akshitaguptaapeejay@gmail.com>
2017-08-17 20:30:00 +0530

    timer added

--- js/spin.js
+++ js/spin.js
@@ -189,7 +189,16 @@
         }
 
    
-    var interval;
+   function myFunction() {
+    var x = document.getElementById('question');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+   
+  /*  var interval;
     var minutes = 45;
     var seconds = 0;
     window.onload = function() {
@@ -219,4 +228,48 @@
             el.innerHTML ='Count Down: '+ minute_text + ' ' + second_text ;
             seconds--;
         }, 1000);
+    } */
+
+	
+<!-- Timer Js-->
+function getTimeRemaining(endtime) {
+  var t = new Date("Oct 5, 2017 15:37:25").getTime() -new Date().getTime();
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
     }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date("Oct 4, 2017 15:37:25").getTime();
+initializeClock('clockdiv', deadline);
