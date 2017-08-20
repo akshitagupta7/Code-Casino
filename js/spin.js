ca69c66f956bab5d72ac5d5e661f83f477a58055
Shinjanee <shinjanee081btcse16@igdtuw.ac.in>
2017-08-20 20:45:13 +0530

    timer changed

--- js/spin.js
+++ js/spin.js
@@ -236,12 +236,8 @@ function getTimeRemaining(endtime) {
   var t = new Date("Oct 5, 2017 15:37:25").getTime() -new Date().getTime();
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
   return {
     'total': t,
-    'days': days,
-    'hours': hours,
     'minutes': minutes,
     'seconds': seconds
   };
@@ -249,21 +245,18 @@ function getTimeRemaining(endtime) {
 
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
   var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
     if (t.total <= 0) {
       clearInterval(timeinterval);
+	  else
+		  window.location="end.html";
     }
   }
 
